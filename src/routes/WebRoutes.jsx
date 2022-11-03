ed7d8055449f289f6b03134bb21ed7f2b552ab45
Aristide NIYONGABO <niyongaboaristide17@gmail.com>
2022-11-04 01:03:22 +0200

    added authentication forms

--- src/routes/WebRoutes.jsx
+++ src/routes/WebRoutes.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 import { Routes, Route } from 'react-router-dom';
+import ChangePassword from '../pages/ChangePassword';
+import ForgotPassword from '../pages/ForgotPassword';
 import Photos from '../pages/Photos';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const WebRoutes = () => (
 
 	<Routes>
 		<Route path='/' element={<Photos/>} />
+		<Route path='/login' element={<SignIn/>} />
+		<Route path='/signup' element={<SignUp/>} />
+		<Route path='/forgot-password' element={<ForgotPassword/>} />
+		<Route path='/change-password' element={<ChangePassword/>} />
 	</Routes>
 )
 
